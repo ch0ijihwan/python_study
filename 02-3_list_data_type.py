06c4ba09026e103b9814cfb77dd40c55d3ded492
jihwan <cjh87467@gmail.com>
2020-12-23 02:47:23 +0900

    list data type.py

--- 02-3_list_data_type.py
+++ 02-3_list_data_type.py
@@ -96,3 +96,56 @@ print(a)
 #리스트 관련 함수
 
 #1. 리스트에 요소 추가(append)
+a = [1,2,3]
+a.append(4)
+print(a)
+
+a.append([5,6])
+print(a)
+
+#2. 리스트 정렬(sort)
+a = [1,5,2,4]
+a.sort()
+print(a)
+
+a = ['a','d','c','b','e']
+a.sort()
+print(a)
+
+#3. 리스트 뒤집기(reverse)
+a = ['a','c','b']
+a.reverse()
+print(a)
+
+
+#4. 위치 반환(index)
+a = [1,2,3]
+print(a.index(3))
+print(a.index(1))
+
+#5.리스트에 요소 삽입(insert)
+a =[1,2,3]
+a.insert(0,4) #a[0]위치에 4 삽입
+print(a)
+
+#6. 리스트 요소 제거(remove)
+a = [1,2,3,1,2,3]
+a.remove(3) #오소의 첫번째로 나오는 3 삭제
+print(a)
+
+#7. 리스트 요소 끄집어 내기
+a = [1,2,3]
+a.pop()
+print(a)
+
+#8.리스트에 포함된 요소 x의 개수 세기(count)
+a = [1,2,3,1]
+print(a.count(1))
+
+#9.리스트 확장(extend) 
+a = [1,2,3]
+a.extend([4,5])
+print(a)
+b=[6,7]
+a.extend(b)
+print(a)
