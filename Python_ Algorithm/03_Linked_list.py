ced57fd837b3c44a1b05958c891d6ed9d1e80a74
jihwan <cjh87467@gmail.com>
2021-01-01 17:35:43 +0900

    if

--- Python_ Algorithm/03_Linked_list.py
+++ Python_ Algorithm/03_Linked_list.py
@@ -17,13 +17,34 @@ def init_list():#연결리스트를 초기화 하는 기능
     node_B.next = node_C
     node_C.next = node_D
 
-def print_list():
+def print_list(): #연결리스트를 출력해줌
     global node_A
     node = node_A
     while node:
         print(node.data)
         node = node.next
     print
+
+def delete_node(del_data):
+    global node_A
+    pre_node = node_A
+    next_node = pre_node.next
+
+    if pre_node(del_data):
+        node_A = next_node
+        del pre_node
+        return
+
+    while next_node:
+        if next_node.data == del_data:
+            pre_node.next = next_node.next
+            del next_node
+            break
+        pre_node = next_node
+        next_node = next_node.next
+
+def insert_node(data):        
+//
 
 if __name__ =='__main__':
     init_list()
